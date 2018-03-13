347e77c66550f57bc106f24b4f13031a37f5808e
Liudmila <mizuxira@yandex.ru>
2018-03-13 10:30:49 +0300

    longest-consecutive-sequence:feature: the length of the longest consecutive elements sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+    let c;
+    var set = new Set();
+    for (var i = 0; i < array.length; i++) {
+        if (!set.has(array[i])) {
+            set.add(array[i]);
+        }
+    }
+    var max = 0;
+    var count = 0;
+    for (i in array) {
+        c = array[i];
+        if (set.has(array[i])) {
+            count = 1;
+            set.delete(array[i]);
+        }
+        while (set.has(c - 1)) {
+            if (set.has(c - 1)) {
+                count++;
+                set.delete(c - 1);
+                c -= 1;
+            }
+        }
+        c = array[i];
+        while (set.has(c + 1)) {
+            if (set.has(c + 1)) {
+                count++;
+                set.delete(c + 1);
+                c += 1;
+            }
+        }
+        if (count > max) {
+            max = count;
+        }
+    }
+    return max;
+};
